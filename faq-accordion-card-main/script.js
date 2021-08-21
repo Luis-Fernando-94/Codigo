efffef41d91a8698020e88508ac2e9f2c371fb1f
Luis-Fernando-94 <88594810+Luis-Fernando-94@users.noreply.github.com>
2021-08-21 14:48:30 -0300

    Add files via upload

---
+++ faq-accordion-card-main/script.js
@@ -0,0 +1,61 @@
+
+let ask1 = document.getElementsByClassName('ask1')[0];
+let ask2 = document.getElementsByClassName('ask2')[0];
+let ask3 = document.getElementsByClassName('ask3')[0];
+let ask4 = document.getElementsByClassName('ask4')[0];
+let ask5 = document.getElementsByClassName('ask5')[0];
+
+function mostrar1(e) {
+    ask1.hidden = false;
+    ask1.style.fontWeight = '400';
+    e.style.fontWeight = '700';
+    e.style.color = 'black';
+}
+function esconder1(e) {
+    ask1.hidden = true;
+    e.style.fontWeight = '400'
+}
+
+function mostrar2(e) {
+    ask2.hidden = false;
+    ask2.style.fontWeight = '400';
+    e.style.fontWeight = '700';
+    e.style.color = 'black';
+}
+function esconder2(e) {
+    ask2.hidden = true;
+    e.style.fontWeight = '400'
+}
+
+function mostrar3(e) {
+    ask3.hidden = false;
+    ask3.style.fontWeight = '400';
+    e.style.fontWeight = '700';
+    e.style.color = 'black';
+}
+function esconder3(e) {
+    ask3.hidden = true;
+    e.style.fontWeight = '400'
+}
+
+function mostrar4(e) {
+    ask4.hidden = false;
+    ask4.style.fontWeight = '400';
+    e.style.fontWeight = '700';
+    e.style.color = 'black';
+}
+function esconder4(e) {
+    ask4.hidden = true;
+    e.style.fontWeight = '400'
+}
+
+function mostrar5(e) {
+    ask5.hidden = false;
+    ask5.style.fontWeight = '400';
+    e.style.fontWeight = '700';
+    e.style.color = 'black';
+}
+function esconder5(e) {
+    ask5.hidden = true;
+    e.style.fontWeight = '400'
+}
